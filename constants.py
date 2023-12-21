df810f2b46f15308c9dd492858de326e1adfaec9
Clint Lohr <lohrcl@oregonstate.edu>
2023-12-20 23:05:22 -0700

    Update constants.py

--- constants.py
+++ constants.py
@@ -10,11 +10,11 @@ BIKES = "bikes"
 COMPONENTS = "components"
 
 # Randomly generated secret key used for session dictionary access
-SECRET_KEY = "8583fae43a4f8a909999f256b2a37ff32cc0ecce6861f16673c50af391452722"
+SECRET_KEY = ""
 
 # Client information for Auth0
-CLIENT_ID = "4DQ6Ow9vyvwSHFYBUgsEW3LFDpzsdhPT"
-CLIENT_SECRET = "7xtmiYnWhY2LpCRZu3I_QRrMMfiMKDFa-PtM0_bv9KuhXQEfr1QnoMWL5LnAU0Qs"
+CLIENT_ID = ""
+CLIENT_SECRET = ""
 
 # Domain name for Auth0 authorization
 DOMAIN = "portfolio-lohrcl.us.auth0.com"
